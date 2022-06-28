b54600154d92fc82ffa1e9e59f8e04a4143c042c
Justin Davis <justintdavis8@gmail.com>
2022-06-28 01:46:21 -0500

    Update Playground.java

--- java-playground/src/main/java/com/charie/Playground.java
+++ java-playground/src/main/java/com/charie/Playground.java
@@ -3,6 +3,12 @@ package com.charie;
 public class Playground {
 
     public static void main(String[] args) {
+        int x = 3;
+        String k = "heyo";
+        boolean car = true;
 
+        System.out.println(car);
+        System.out.println(k);
+        System.out.println(x);
     }
 }
